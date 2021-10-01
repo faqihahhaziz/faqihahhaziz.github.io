ea3188e7d50bec8f5f7d5abff13ca1949be1e911
Faqihah Haziz <faqihahhaziz@gmail.com>
2021-10-01 21:57:36 +0800

    21:57

---
+++ geolocation/js/location.js
@@ -0,0 +1,17 @@
+let elLocate = document.getElementById("btnLocate")
+elLocate.addEventListener("click",function(){
+    
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(function(position){
+            let elLat = document.getElementById("lat")
+            let elLong = document.getElementById("long")
+        
+            elLat.innerHTML = "Latitude:" + position.coords.latitude
+            elLong.innerHTML = "Longitude:" + position.coords.longitude
+        })
+        
+    }else{
+        alert("Gelocation is not supported")
+    }
+})
