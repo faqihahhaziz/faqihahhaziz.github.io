64923aa49f419b7617335c67026cd091c88ad22a
Faqihah Haziz <faqihahhaziz@gmail.com>
2021-10-17 22:20:04 +0800

    factorialjs added

---
+++ assignment04/js/factorial.js
@@ -0,0 +1,11 @@
+function factorial(){  
+var i, num, f;  
+f = 1;  
+num = document.getElementById("num").value;  
+for(i = 1; i <= num; i++)    
+{  
+f = f * i;  
+}  
+i = i - 1;    
+document.getElementById("result").innerHTML = "The factorial of the number " + i + " is: " + f ;  
+}  
