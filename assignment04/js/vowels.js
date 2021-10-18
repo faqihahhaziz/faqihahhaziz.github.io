e8f0b860cfd5c1bfa34670b51457b4b952fd85a9
Faqihah Haziz <faqihahhaziz@gmail.com>
2021-10-18 11:26:19 +0800

    vowelsjs added

---
+++ assignment04/js/vowels.js
@@ -0,0 +1,19 @@
+
+    function GetVowels() {
+      var str = document.getElementById('inputText').value;
+      var count = 0, total_vowels="";
+      for (var i = 0; i < str.length; i++) {
+      if (str.charAt(i).match(/[a-zA-Z]/) != null) {
+      // findVowels
+      if (str.charAt(i).match(/[aeiouAEIOU]/))
+      {
+      total_vowels = total_vowels + str.charAt(i);
+      count++;
+      }
+      }
+      }
+      document.getElementById('vowels').value = total_vowels;
+      document.getElementById('vcount').value = count;
+      }
+    
+   
